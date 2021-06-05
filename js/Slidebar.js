b10bcd6438c20fd33f4ffd0ce7796b588ed5ab7c
Bui Quang Vinh <kytu147258@gmail.com>
2021-06-05 10:05:50 +0700

    music

---
+++ js/Slidebar.js
@@ -0,0 +1,39 @@
+// const $template = document.createElement("template");
+// $template.innerHTML = `<div class="slidebar">
+//         <img src="./img/logo3.png" alt="logo" />
+//         <div class="menu">
+//           <ul>
+//             <a href="./home.html" id="home">
+//               <li>Home</li>
+//             </a>
+//             <a href="./record.html">
+//               <li>Record chart</li>
+//             </a>
+//             <a href="./video.html">
+//               <li>Music Video</li>
+//             </a>
+//             <a href="./singer.html">
+//               <li>Singer</li>
+//             </a>
+//           </ul>
+//         </div>
+
+//         <div class="social-list">
+//           <a href=""><i class="fab fa-facebook"></i></a>
+//           <a href=""> <i class="fab fa-youtube"></i></a>
+//           <a href=""><i class="fab fa-instagram"></i></a>
+//           <a href=""><i class="fab fa-google"></i></a>
+//           <a href=""><i class="fab fa-twitter"></i></a>
+//         </div>
+//       </div>
+
+// `;
+
+// export default class Slidebar extends HTMLElement {
+//   constructor() {
+//     super();
+//     this.appendChild($template.content.cloneNode(true));
+//   }
+// }
+
+// window.customElements.define("my-slidebar", Slidebar);
